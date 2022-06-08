bb25132d4cb97286288ffba6a9c256d86a855e6c
mmajd <mabulmagd55@gmail.com>
2022-06-08 19:42:00 +0200

    restructured & enhanced

--- src/bridgepattern/bridge/Academic.java
+++ src/bridgepattern/bridge/Academic.java
@@ -3,10 +3,10 @@ package bridgepattern.bridge;
 public class Academic implements IFormatter {
     @Override
     public void format(String title, String content) {
-        System.out.printf("%s\n", "-".repeat(22));
-        System.out.format("%-3s %s %3s\n", "*", "Academia Paper", "*");
-        System.out.printf("%s\n", "-".repeat(22));
-        System.out.format("%s\n", title);
-        System.out.format("%s\n", content);
+        System.out.printf("%s%n", "-".repeat(22));
+        System.out.format("%-3s %s %3s%n", "*", "Academic Paper", "*");
+        System.out.printf("%s%n", "-".repeat(22));
+        System.out.format("%s%n", title);
+        System.out.format("%s%n", content);
     }
 }
